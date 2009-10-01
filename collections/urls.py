2dd5b15e8c8dc64eb38f3047e32f48b6b914cc02
Stepan Rakhimov <strakh@macbook.local>
2009-10-01 11:00:54 +0400

    channel/pack concepts, untested

--- collections/urls.py
+++ collections/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^publish/$', 'django.views.generic.simple.direct_to_template', dict(template='tweets/publish_settings.html')),
     url(r'^waw/$', 'tweets.views.waw', name='tweets_waw'),
     url(r'^manage/(?P<group_id>\d+)/delete/(?P<source_id>\d+)/$', 'tweets.views.manage_groups', name='tweets_manage_delete_source'),
     url(r'^manage/(?P<group_id>\d+)/$', 'tweets.views.manage_groups', name='tweets_manage_group'),
